f3e79671df3afd3fa9e647537df7441eaa684e40
daiya-elu <tenbeyjun@icloud.com>
2019-07-18 23:43:22 +0900

    sp create

--- surf/js/script.js
+++ surf/js/script.js
@@ -1,16 +1,15 @@
 jQuery(function ($) {
-  $('#top-image').bgswitcher({
-    images: ['img/top-img1.png', 'img/top-img2.png', 'img/top-img3.png', 'img/top-img4.png', 'img/top-img5.png'],
-    interval: 4000,
-    effect: "fade",
-    duration: 1000,
-    easing: "swing",
-  });
+    $('#top-image').bgswitcher({
+      images: ['img/top-img1.png', 'img/top-img2.png', 'img/top-img3.png', 'img/top-img4.png', 'img/top-img5.png'],
+      interval: 4000,
+      effect: "fade",
+      duration: 1000,
+      easing: "swing",
+    });
 });
 
 $(function() {
   var topBtn = $('#page_top');
-
   topBtn.hide();
 
   $(window).scroll(function () {
